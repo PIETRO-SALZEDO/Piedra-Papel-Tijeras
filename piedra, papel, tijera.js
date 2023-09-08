5f5a6aa6d7d0b9dff4eaadb12ed56b7e69780ef4
PIETRO-SALZEDO <140338857+PIETRO-SALZEDO@users.noreply.github.com>
2023-09-08 13:20:40 -0500

    Add files via upload

---
+++ piedra, papel, tijera.js
@@ -0,0 +1,13 @@
+var partidas = parseInt(prompt("ingrese el numerode partidas a jugar solo numeros impar"))
+var victoriaU, victoriasCpu = 0;
+
+var usuario = prompt(" Escoja el numeropara Jugar: 1 es Piedra, 2 es Papel, 3 es Tijera?")
+var cpu = math. ramdom(1,3);
+do{
+if (usuario == cpu){
+    console.log("empate")
+} else if (usuario == 1 && cpu == 3){
+
+} 
+} while( victoriaU < 4)
+
